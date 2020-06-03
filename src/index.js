054641d0f60e2c1cc8b1e7316478d12621d11345
Harshal Lele <harshallele97@gmail.com>
2020-06-03 10:14:06 +0530

    added comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+//object where all subscribers are stored
 const bus = {};
 
 module.exports = {
+    /**
+     * @param {string} event - event name
+     * @param  {...any} args - variable number of arguments that you want to pass to the subscribing method
+     */
     publish: function(event, ...args){
         if(bus[event]){
             Object.values(bus[event]).forEach(callback => {
@@ -13,6 +18,12 @@ module.exports = {
         
     },
 
+    /**
+     * @param {string} event - event name
+     * @param {Function} callback - method to run when event is fired
+     * @param {boolean} once - if true, method will be run only for the first publish
+     * @returns {Number} id - the id of the subscription. used for unsubscribing
+     */
     subscribe: function(event,callback,once = false){
         if(!callback) return;
         
@@ -36,6 +47,10 @@ module.exports = {
         return id;
     },
 
+    /** 
+     * @param {string} event - event name
+     * @param {Number} id - id of the method that you want to remove
+     */
     unsubscribe: function(event,id){
         for (const key in bus[event]){
             if(key == id){
@@ -44,14 +59,27 @@ module.exports = {
         }
     },
 
+    /**
+     * @param {string} event - event name
+     * @returns {Object} bus[event] - Object containing all subscribers registered for passed event
+     */
     getAllSubscribers: function(event){
         return bus[event];
     },
 
+    /**
+     * @param {string} event - event name
+     * @param {Function} callback - function that will be run when passed in event is published
+     * @returns {Number} id - the id of the subscription
+     */
     subOnce: function(event,callback){
         return this.subscribe(event,callback,true);
     },
 
+    /**
+     * @param {string} event - event name
+     * @param  {...any} args - variable number of arguments that you want to pass to the subscribing method
+     */
     pubSticky: function(event,...args){
         if(!bus.sticky) bus.sticky = {};
         if(!bus.sticky[event]) bus.sticky[event] = [];
@@ -59,6 +87,9 @@ module.exports = {
         bus.sticky[event].push({args:args});
     },
 
+    /**
+     * @returns {Object} bus - object where all subscribers are stored
+     */
     getBus: function(){
         return bus;
     }
